3cfd09f8f860f2636c6ccdccfb3d0d65287653f6
MhdMoh <mhd.nidal.mhd@gmail.com>
2024-12-17 12:25:53 +0100

    Initial Commit

---
+++ Nimbus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nimbus
+//
+//  Created by Mohamad Mohamad on 17/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nimbus/NimbusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimbusApp.swift
+//  Nimbus
+//
+//  Created by Mohamad Mohamad on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct NimbusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
